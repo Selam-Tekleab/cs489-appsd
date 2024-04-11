32a27f9c99e8886bf8e77d856badf1aaaf4fcaff
selam-Tekleab <selam.tekleab6@gmail.com>
2024-04-11 18:50:12 -0500

    lesson 6 assignment on data persistance

---
+++ ADS_JPA-main/src/main/java/edu/miu/cs/cs489/lesson6/citylibraryapp/service/IAddressService.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs489.lesson6.citylibraryapp.service;
+
+import edu.miu.cs.cs489.lesson6.citylibraryapp.models.Address;
+
+public interface IAddressService {
+    Address save(Address address);
+}
